067af71eb8cc519106d25135643279f67e80d9c5
glenvt18 <glenvt18@gmail.com>
2017-05-18 06:03:32 +0300

    Make compatible with VDR 2.3.4

--- epghandler.h
+++ epghandler.h
@@ -15,6 +15,9 @@ class cEpgfixerEpgHandler : public cEpgHandler
 {
 public:
   cEpgfixerEpgHandler(void) {};
+#if VDRVERSNUM >= 20304
+  virtual bool BeginSegmentTransfer(const cChannel *Channel, bool Dummy) { return true; }
+#endif
   virtual bool HandleEvent(cEvent *Event);
   virtual bool IgnoreChannel(const cChannel *Channel);
   virtual bool FixEpgBugs(cEvent *Event);
